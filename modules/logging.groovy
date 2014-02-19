dd5040590789e80bc7e1b0444cbfec5ee15fea28
Simon Templer <simon@templer.cc>
2014-02-19 19:38:06 +0100

    updated logging module example
    Eliminiated project.with call as it is no longer needed.

--- modules/logging.groovy
+++ modules/logging.groovy
@@ -4,38 +4,36 @@ import org.gradle.api.artifacts.DependencyResolveDetails
  * Adds slf4j and logback to the platform
  */
 def slf4jAndLogback(String slf4jVersion = '1.7.5', String logbackVersion = '1.0.13') {
-	project.with {
-		configurations {
-			platform {
-				// resolution configuration for logging bridges (... over slf4j)
-				
-				resolutionStrategy.eachDependency { DependencyResolveDetails details ->
-					if (details.requested.name == 'commons-logging') {
-						// prefer 'jcl-over-slf4j' over 'commons-logging'
-						details.useTarget "org.slf4j:jcl-over-slf4j:${slf4jVersion}"
-					}
+	configurations {
+		platform {
+			// resolution configuration for logging bridges (... over slf4j)
+			
+			resolutionStrategy.eachDependency { DependencyResolveDetails details ->
+				if (details.requested.name == 'commons-logging') {
+					// prefer 'jcl-over-slf4j' over 'commons-logging'
+					details.useTarget "org.slf4j:jcl-over-slf4j:${slf4jVersion}"
 				}
-				
-				resolutionStrategy.eachDependency { DependencyResolveDetails details ->
-					if (details.requested.name == 'log4j' && details.requested.group == 'log4j') {
-						// prefer 'log4j-over-slf4j' over 'log4j'
-						details.useTarget "org.slf4j:log4j-over-slf4j:${slf4jVersion}"
-					}
+			}
+			
+			resolutionStrategy.eachDependency { DependencyResolveDetails details ->
+				if (details.requested.name == 'log4j' && details.requested.group == 'log4j') {
+					// prefer 'log4j-over-slf4j' over 'log4j'
+					details.useTarget "org.slf4j:log4j-over-slf4j:${slf4jVersion}"
 				}
 			}
 		}
+	}
+	
+	platform {
+		// slf4j
+		bundle "org.slf4j:slf4j-api:${slf4jVersion}"
+		bundle "org.slf4j:slf4j-ext:${slf4jVersion}"
 		
-		platform {
-			// slf4j
-			bundle "org.slf4j:slf4j-api:${slf4jVersion}"
-			bundle "org.slf4j:slf4j-ext:${slf4jVersion}"
-			
-			// jul adapter
-			bundle "org.slf4j:jul-to-slf4j:${slf4jVersion}"
-			
-			// logback
-			bundle "ch.qos.logback:logback-classic:${logbackVersion}"
-			bundle "ch.qos.logback:logback-core:${logbackVersion}"
-		}
+		// jul adapter
+		bundle "org.slf4j:jul-to-slf4j:${slf4jVersion}"
+		
+		// logback
+		bundle "ch.qos.logback:logback-classic:${logbackVersion}"
+		bundle "ch.qos.logback:logback-core:${logbackVersion}"
 	}
 }

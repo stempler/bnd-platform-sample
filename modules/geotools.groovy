b75f05d4083de70bcef5b9401f278045abe21ca4
Simon Templer <simon@templer.cc>
2014-02-21 18:19:12 +0100

    started merge implementation

--- modules/geotools.groovy
+++ modules/geotools.groovy
@@ -1,44 +1,45 @@
-def defaultModules = [
-	'gt-api',
-//	'gt-coverage',
-//	'gt-cql',
-//	'gt-data',
-	'gt-epsg-hsql',
-//	'gt-feature-pregeneralized',
-	'gt-geojson',
-	'gt-main',
-//	'gt-metadata',
-//	'gt-process',
-//	'gt-property',
-//	'gt-referencing',
-//	'gt-referencing3D',
-//	'gt-render',
-//	'gt-validation',
-//	'gt-wfs',
-//	'gt-wms',
-//	'gt-xml',
-//	'gt-xsd-core',
-//	'gt-xsd-fes',
-//	'gt-xsd-filter',
-//	'gt-xsd-gml2',
-//	'gt-xsd-gml3',
-//	'gt-xsd-kml',
-//	'gt-xsd-ows',
-//	'gt-xsd-sld',
-//	'gt-xsd-wcs',
-//	'gt-xsd-wfs',
-//	'gt-xsd-wms',
-//	'gt-xsd-wps',
-	'gt-shapefile'
-]
-
 /**
  * Adds a combined geotools bundle and an opengis bundle to the platform.
  */
 def geotools(String geotoolsVersion = '10.4', String bundleVersion = '10.4.0.combined',
-	def modules = defaultModules) {
+	def modules = [
+		'gt-api',
+//		'gt-coverage',
+//		'gt-cql',
+//		'gt-data',
+		'gt-epsg-hsql',
+//		'gt-feature-pregeneralized',
+		'gt-geojson',
+		'gt-main',
+//		'gt-metadata',
+//		'gt-process',
+//		'gt-property',
+//		'gt-referencing',
+//		'gt-referencing3D',
+//		'gt-render',
+//		'gt-validation',
+//		'gt-wfs',
+//		'gt-wms',
+//		'gt-xml',
+//		'gt-xsd-core',
+//		'gt-xsd-fes',
+//		'gt-xsd-filter',
+//		'gt-xsd-gml2',
+//		'gt-xsd-gml3',
+//		'gt-xsd-kml',
+//		'gt-xsd-ows',
+//		'gt-xsd-sld',
+//		'gt-xsd-wcs',
+//		'gt-xsd-wfs',
+//		'gt-xsd-wms',
+//		'gt-xsd-wps',
+		'gt-shapefile'
+	]) {
 	
 	repositories {
+		maven {
+			url 'http://download.java.net/maven/2'
+		}
 		maven {
 			url 'http://download.osgeo.org/webdav/geotools/'
 		}
@@ -49,9 +50,9 @@ def geotools(String geotoolsVersion = '10.4', String bundleVersion = '10.4.0.com
 	
 	platform {
 		// opengis
-		bundle "org.geotools:gt-opengis:${geotoolsVersion}" {
+		bundle "org.geotools:gt-opengis:${geotoolsVersion}", {
 			bnd {
-				symbolicName = 'org.opengis' //XXX not yet supported
+				symbolicName = 'org.opengis'
 			}
 		}
 		
@@ -63,7 +64,7 @@ def geotools(String geotoolsVersion = '10.4', String bundleVersion = '10.4.0.com
 		}
 		
 		// geotools bundle
-		merge { //XXX not yet supported
+		merge {
 			match {
 				it.group == 'org.geotools' && it.name != 'gt-opengis'
 			}

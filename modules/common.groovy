cd0c9b07ce48be5dadb1e199425a02e05ff75816
Simon Templer <simon@templer.cc>
2014-03-25 22:44:07 +0100

    removed obsolete config, add platformaux sample

--- modules/common.groovy
+++ modules/common.groovy
@@ -6,40 +6,17 @@ platform {
 	// the default bnd configuration does not apply to Jars that are already bundles
 	// (to protect the existing imports and exports)
 	bnd {
-		// sun.* and com.sun.* packages are optional by default
-		optionalImport 'sun.*', 'com.sun.*',
-			// unit tests only dependencies should be optional as well
-			'junit.framework', 'org.junit'
+		// unit test only dependencies should be optional
+		optionalImport 'junit.framework', 'org.junit'
 	}
 	
 	// individual bundle configurations
-	
-	bnd 'jdom:jdom', {
-		optionalImport(
-			'oracle.xml.parser',
-			'oracle.xml.parser.v2',
-			'org.jaxen',
-			'org.jaxen.jdom'
-		)
-	}
 	
 	bnd(group: 'net.sf.json-lib', name: 'json-lib') {
 		// optional imports the `manual way`
 		instruction 'Import-Package',
 			'nu.xom;resolution:=optional,org.apache.oro.text.regex;resolution:=optional,' +
 			(properties['Import-Package']?:'*')
-	}
-	
-	bnd group: 'org.apache.ant', name: 'ant', {
-		optionalImport 'gnu.gcj', 'kaffe.util', 'org.apache.harmony.luni.util', 'weblogic'
-	}
-	
-	bnd group: 'org.codehaus.groovy.modules.http-builder', name: 'http-builder', {
-		optionalImport 'com.google.appengine.api.urlfetch', 'oauth.signpost', 'oauth.signpost.*'
-	}
-	
-	bnd group: 'org.iq80.snappy', name: 'snappy', {
-		optionalImport 'org.apache.hadoop.io.compress'
 	}
 	
 	bnd group: 'commons-configuration', name: 'commons-configuration', {
